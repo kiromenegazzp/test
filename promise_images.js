7496026c8850a0c0212e048573dfb430c07a4500
Kirill <html@i-vengo.com>
2017-09-08 17:58:42 +0300

    promise images

--- promise_images.js
+++ promise_images.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let images = [
-    'http://d33wubrfki0l68.cloudfront.net/f35d49d959deb5bfd7deb80c2668128367e2917c/eb35e/images/logo.svg',
+    'http://d33wubrfki0l68.cloudfront.net/a831feb156a865dbaff01e2deac2b13283d151e5/8885f/images/users/cloudflare.png',
     'http://d33wubrfki0l68.cloudfront.net/beabc2ed6b1c8165942660821459c8f52f9cfb50/99e70/images/users/yahoo.png'
 ];
 
@@ -9,13 +9,15 @@ let downloadImage = src => {
     return new Promise((resolve, reject) => {
         let img = new Image();
         img.src = src;
-        img.onload = resolve(`loaded ${img.src}`);
-        img.onerror = reject('failed');
+        img.onload = resolve(img.src);
+        img.onerror = e => {
+            reject(e);
+        };
     });
 };
 
-Promise.all([images.map(downloadImage)])
+Promise.all(images.map(downloadImage))
 .then(
-results => console.log(results),
-error => console.log(error)
+    results => console.log(results),
+    error => console.log(error)
 );
